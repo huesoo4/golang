26a9e456ee3f3a161c315c1c694fe87301082b14
huesoo4 <jlophue144@g.educaand.es>
2026-06-30 17:23:40 +0200

    Toma de contacto con la estructura básica en Golang.

---
+++ 00-10-primer-contacto/estructura-basica.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("==============================")
+	fmt.Println("Perfil DevOps")
+	fmt.Println("==============================")
+	fmt.Println("Nombre: Jose Manuel")
+	fmt.Println("Edad: 25")
+	fmt.Println("Ciudad: Sevilla")
+	fmt.Println("Objetivo: Alcanzar nivel para perfil DevOps")
+	fmt.Println("==============================")
+}
+
+/*
+Este programa en Go imprime por consola un perfil con información personal.
+- Usa package para indicar que es un programa ejecutable.
+- Importa "fmt" para poder mostrar el texto.
+- Func main() ejecuta varias instrucciones.
+- fmt.Println() escribe las líneas dentro de la función main en la terminal.
+*/
