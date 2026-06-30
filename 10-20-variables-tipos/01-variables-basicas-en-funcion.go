aa014925b7315888bb52306abd0da229b472a841
huesoo4 <jlophue144@g.educaand.es>
2026-06-30 18:50:04 +0200

    Variables básicas en Go dentro de la función.

---
+++ 10-20-variables-tipos/01-variables-basicas-en-funcion.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Jose Manuel"
+	edad := 25
+	altura := 1.70
+	estudiando := false
+
+	fmt.Println("Nombre:", nombre)
+	fmt.Println("Edad:", edad)
+	fmt.Println("Altura:", altura)
+	if estudiando {
+		fmt.Println("Estudiando: Sí")
+	} else {
+		fmt.Println("Estuandianto: No")
+	}
+}
